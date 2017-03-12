bc57b58679baeb29cf1b77b39e732030a6a537d2
longbai <slongbai@gmail.com>
2017-03-12 20:35:15 +0800

    base64 done

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,14 @@
 #![deny(warnings)]
 extern crate crc;
+extern crate rustc_serialize;
 
 use self::crc::crc32;
+use self::rustc_serialize::base64::{ToBase64, URL_SAFE};
 
 pub fn crc32_bytes(bytes: &[u8]) -> u32 {
 	return crc32::checksum_ieee(bytes);
 }
 
-
+pub fn base64_encode(bytes: &[u8]) -> String {
+	return bytes.to_base64(URL_SAFE);
+}
--- tests/utils_test.rs
+++ tests/utils_test.rs
@@ -6,3 +6,8 @@ use qiniu::utils;
 fn crc32_bytes() {
     assert_eq!(utils::crc32_bytes(b"Hello, World!"), 3964322768);
 }
+
+#[test]
+fn base64_encode(){
+	assert_eq!(&*(utils::base64_encode("你好/+=".as_bytes())), "5L2g5aW9Lys9");
+}
